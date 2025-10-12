cc8150b9501abc71b3d4cc49456de47dbdc3a89a
Туманов Захар Андреевич ИП-514 <zahar.tumanow@yandex.ru>
2025-10-12 15:40:56 +0700

    попытка 6

--- src/exercise1.c
+++ src/exercise1.c
@@ -12,10 +12,10 @@ int main(int argc,char** argv ){
     }
     double avg=sum/10;
     if(avg >= 0){
-        avg = (int)(avg*100 + 0.5)/100.0;
+        avg = (int)(avg*100 + 0.5001)/100.0;
     }
     else{
-        avg = (int)(avg*100 - 0.5)/100.0;
+        avg = (int)(avg*100 - 0.5001)/100.0;
     }
     printf("%.2f\n",avg);
 
